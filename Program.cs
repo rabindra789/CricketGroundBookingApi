de23d9ba9496f4de05c24be203f2776a2db2fec2
rabindra789 <rabindrameher116@gmail.com>
2026-05-10 19:26:36 +0530

    fix: enable swagger in production

--- Program.cs
+++ Program.cs
@@ -84,11 +84,8 @@ builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseForwardedHeaders(new ForwardedHeadersOptions
 {
